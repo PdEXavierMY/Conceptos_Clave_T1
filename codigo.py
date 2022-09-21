b874a386978fc1f6813cefeb11ae7e4c445b1947
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-09-21 16:59:42 +0200

    primera parte

---
+++ codigo.py
@@ -0,0 +1,38 @@
+from turtle import pu
+import math
+
+
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def constructor(a=0, b=0):
+        punto = [a, b]
+        return punto
+    
+    def string(self, Punto):
+        print("("+str(Punto[0])+", "+str(Punto[1])+")")
+
+    def cuadrante(punto):
+        if punto[0]>0 and punto[1]>0:
+            print("Pertenece al cuadrante 1")
+        elif punto[0]<0 and punto[1]>0:
+            print("Pertenece al cuadrante 2")
+        elif punto[0]>0 and punto[1]<0:
+            print("Pertenece al cuadrante 3")
+        elif punto[0]<0 and punto[1]<0:
+            print("Pertenece al cuadrante 4")
+        elif punto[0]==0:
+            print("Está sobre el eje x")
+        elif punto[1]==0:
+            print("Está sobre el eje y")
+        elif punto[0]==0 and punto[1]==0:
+            print("Está en el eje de coordenadas")
+
+    def vector(Punto1, Punto2):
+        vector = [Punto2[0]-Punto1[0], Punto2[1]-Punto1[1]]
+        return vector
+
+    def distancia(Punto1, Punto2):
+        return math.sqrt(Punto.vector(Punto1, Punto2)[0]**2+Punto.vector(Punto1, Punto2)[1]**2)
